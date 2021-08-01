802666d3242c1f70fbdec4e61ee8b420a4534654
Dovzhenko-olga <73553889+Dovzhenko-olga@users.noreply.github.com>
2021-08-01 03:57:03 +0300

    make hooks

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -1,37 +1,38 @@
-import { Component } from "react";
-import PropTypes from 'prop-types';
+import { useState } from "react";
 import styles from './Form.module.css';
 
-const INITIAL_STATE = {
-  name: '',
-  number: '',
-};
 
-class Form extends Component  {
-  state = { ...INITIAL_STATE };
+export default function Form({onSubmit}) {
+  const [name, setName] = useState('');
+  const [number, setNumber] = useState('');
 
-  static propTypes = {
-    onSubmit: PropTypes.func.isRequired,
+  const handleChange = e => {
+    const { name, value } = e.target;
+    switch (name) {
+      case 'name':
+        setName(value);
+        break;
+      case 'number':
+        setNumber(value);
+        break;
+      default:
+        break;
+    }
+  }
+
+  const reset = () => {
+    setName('');
+    setNumber('');
+  }
+
+  const handleSubmit = e => {
+    e.preventDefault();    
+    onSubmit({name, number});
+    reset();
   };
 
-  handleChange = e => {
-    const { name, value } = e.currentTarget
-    this.setState({ [name]: value })
-  };
-  
-  handleSubmit = e => {
-    e.preventDefault();
-    this.props.onSubmit(this.state);
-    this.reset();
-  };
-
-  reset = () => {
-    this.setState({ ...INITIAL_STATE });
-  };
-
-  render() {
-    return (
-    <form className={styles.form} onSubmit={this.handleSubmit}>
+  return (
+    <form className={styles.form} onSubmit={handleSubmit}>
       <label>
         <p className={styles.name}>Name</p>
         <input
@@ -39,11 +40,11 @@ class Form extends Component  {
           name="name"
           placeholder="Enter name"
           className={styles.input}
-          value={this.state.name}
+          value={name}
           pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
           title="Имя может состоять только из букв, апострофа, тире и пробелов. Например Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan и т. п."
           required
-          onChange={this.handleChange}
+          onChange={handleChange}
         />
         </label>
         <label>
@@ -52,17 +53,15 @@ class Form extends Component  {
             type="tel"
             name="number"
             placeholder="Enter number"
-            value={this.state.number}
+            value={number}
             className={styles.input}
             pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
             title="Номер телефона должен состоять цифр и может содержать пробелы, тире, круглые скобки и может начинаться с +"
             required
-            onChange={this.handleChange}
+            onChange={handleChange}
           />
         </label>
       <button className={styles.button} type="submit">Add contact</button>
     </form>
-    )}
-};
-
-export default Form;
+    )
+ }
